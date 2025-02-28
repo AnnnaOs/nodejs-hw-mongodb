885f5269ad96ee5284087c773004129b724f9eef
AnnnaOs <preciousanutka@gmail.com>
2025-02-28 10:08:03 +0200

    add status

--- src/server.js
+++ src/server.js
@@ -29,6 +29,7 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -45,6 +46,7 @@ export const setupServer = () => {
     }
 
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
